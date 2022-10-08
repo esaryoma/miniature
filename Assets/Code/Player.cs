eb90d7408f00aa562fff02e21c9dd81307642fe4
Esa Ryömä <esa.ryoma@gmail.com>
2022-10-08 14:11:34 +0300

    Player, SkillCard, Skill code and Player prefab initial files

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int endurance;
+    [SerializeField] private int wounds;
+    [SerializeField] private int resolve;
+    [SerializeField] private List<SkillCard> skillCards;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
