12aca294afa7449d668ee4fc6d6812140381ddaf
Esa Ryömä <esa.ryoma@gmail.com>
2022-10-08 17:29:22 +0300

    Fixed fighter bow; added mage

--- Assets/Code/Character.cs
+++ Assets/Code/Character.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     [SerializeField] public string charName;
-    [SerializeField] private int endurance;
-    [SerializeField] private int wounds;
-    [SerializeField] private int resolve;
+    [SerializeField] public int endurance;
+    [SerializeField] public int wounds;
+    [SerializeField] public int resolve;
 
     // Start is called before the first frame update
     void Start()
