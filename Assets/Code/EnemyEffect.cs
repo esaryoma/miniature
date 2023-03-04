b3f557f62cb38cd38459cb7dd5079cc5b4661f76
Esa Ryömä <esa.ryoma@gmail.com>
2023-03-04 15:37:37 +0200

    Worked on enemy prefabs containing Reactions/Behaviors/Effects

--- Assets/Code/EnemyEffect.cs
+++ Assets/Code/EnemyEffect.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Reaction : MonoBehaviour
+public class EnemyEffect : Effect
 {
-    [SerializeField] public string reactionName;
-
-    [SerializeField] public List<EnemyEffect> effects;
+    [SerializeField] public int resource;
 
     // Start is called before the first frame update
     void Start()
